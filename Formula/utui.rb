832809bf506fc36d906f1096677985ca747f046e
ahmedelkadii <ahmedelkadii@kodeflowstudios.com>
2026-08-31 08:32:05 +0300

    fixed checksum

--- Formula/utui.rb
+++ Formula/utui.rb
@@ -2,7 +2,7 @@ class Utui < Formula
   desc "TUI for managing Unity projects and editor installs"
   homepage "https://github.com/kodeflowstudios/utui"
   url "https://github.com/kodeflowstudios/utui/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "6c094fdb630178313ca0bab4602b9a039b296277a3cb97e13e27984e27c0a3c4"
   license "MIT"
 
   depends_on "rust" => :build
